11ebc0760b48789deb93bfc4d5e8092aead485db
Servicios Digitales <Cetecom@SJAPCA03LC0814.duoc.acad>
2025-06-11 10:23:35 -0400

    Se edita el archivo main

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-num1 = int(input("Ingrese el primer numero: "))
-num2 = int(input("Ingrese el segundo numero: "))
+#Ejercicio 1
 def suma(num1,num2):
     return num1+num2
-nombre = input("Ingrese su nombre: ")
+
+#Ejercicio 2
 def saludo(nombre):
     print(f"Saludos {nombre}")
-numero = int(input("Ingrese un numero: "))
+
+#Ejercicio 3
 def par_impar(numero):
     if numero % 2 == 0:
         print(f"El numero {numero} es par")
     else:
-        print(f"El numero {numero} es impar")        
+        print(f"El numero {numero} es impar")
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))    
 print(suma(num1,num2))
+nombre = input("Ingrese su nombre: ")
 print(saludo(nombre))
+numero = int(input("Ingrese un numero: "))
 par_impar(numero)
